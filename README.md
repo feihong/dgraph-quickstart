6e75ebcb9cb0494fea06e073927012710a79d436
Feihong Hsu <feihong.hsu@gmail.com>
2017-02-02 13:52:00 -0600

    When the query fails, show the error message in red

--- README.md
+++ README.md
@@ -17,6 +17,24 @@ mkvirtualenv -p python3 dgraph
 pip install -r requirements.txt
 ```
 
+If you're using Atom, install the language-graphql package.
+
+## Start database
+
+```
+inv db
+```
+
+## Load example data
+
+When the database is running, run
+
+```
+inv download_example_data
+inv load_example_data
+```
+
 ## Sources
 
 - [Dgraph installation](https://wiki.dgraph.io/Get_Started#Step_1:_Installation)
+- [Load dataset](https://wiki.dgraph.io/Get_Started#Load_dataset)
--- static/main.js
+++ static/main.js
@@ -1,19 +1,27 @@
-$(document).ready(() => {
-  $('textarea.query').focus()
-})
+// $(document).ready(() => {
+//   console.log('focus', $('textarea.query')[0])
+//   $('textarea.query')[0].focus()
+// })
 
-$('textarea.query').on('keypress', evt => {
+$(document).on('keypress', evt => {
   if ((evt.metaKey || evt.ctrlKey) && evt.keyCode === 13) {
     sendQuery(evt.target.value)
   }
 })
 
 let sendQuery = co.wrap(function *(query) {
+  let resultTa = $('textarea.result')
+  resultTa.removeClass('error').text('Executing query...')
+
   let response = yield fetch('http://localhost:8080/query', {
     method: 'POST',
     body: query,
   })
-  let result = yield response.json()
-  let text = JSON.stringify(result, null, 2)
-  $('textarea.result').text(text)
+
+  if (response.status !== 200) {
+    resultTa.addClass('error').text(yield response.text())
+  } else {
+    let text = JSON.stringify(yield response.json(), null, 2)
+    resultTa.text(text)
+  }
 })
